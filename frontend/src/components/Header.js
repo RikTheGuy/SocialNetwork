5e415962a52e4e57e6df15a41b1961a2414efb61
nit270 <51881432+nit270@users.noreply.github.com>
2021-01-08 13:13:21 +0530

    Prototype_Nav

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,30 +1,30 @@
 import React from 'react'
-import {Navbar, Nav, Container, Form, FormControl, Dropdown} from 'react-bootstrap'
+import {Navbar, Nav, Container, Button, Dropdown} from 'react-bootstrap'
 
 
 const Header = () => {
     return (
         <>
-  <Navbar bg="dark" variant="dark">
-  <Container>
+  <Navbar bg="light" variant="light" >
+  <Container className="p-0">
     <Navbar.Brand href="#home"><h1>Quora</h1></Navbar.Brand>
     <Nav className="mr-auto">
-      <Nav.Link href="#home" title="Home"><h1><i class="fa fa-home" aria-hidden="true"></i></h1></Nav.Link>
-      <Nav.Link href="#following" title="Following"><h1><i class="fa fa-address-card" aria-hidden="true"></i></h1></Nav.Link>
-      <Nav.Link href="#answer" title="Answer"><h1><i class="fa fa-pencil" aria-hidden="true"></i></h1></Nav.Link>
-      <Nav.Link href="#spaces" title="Spaces"><h1><i class="fa fa-users" aria-hidden="true"></i></h1></Nav.Link>
-      <Nav.Link href="#notifications" title="Notifications"><h1><i class="fa fa-bell" aria-hidden="true"></i></h1></Nav.Link>
+      <Nav.Link href="#home" title="Home" className="Icon_Hover" ><i class="fa fa-home fa-2x" aria-hidden="true"></i></Nav.Link>
+      <Nav.Link href="#following" title="Following" className="Icon_Hover"><i class="fa fa-address-card fa-2x" aria-hidden="true"></i></Nav.Link>
+      <Nav.Link href="#answer" title="Answer" className="Icon_Hover"><i class="fa fa-edit fa-2x" aria-hidden="true"></i></Nav.Link>
+      <Nav.Link href="#spaces" title="Spaces" className="Icon_Hover"><i class="fa fa-users fa-2x" aria-hidden="true"></i></Nav.Link>
+      <Nav.Link href="#notifications" title="Notifications" className="Icon_Hover"><i class="fa fa-bell fa-2x" aria-hidden="true"></i></Nav.Link>
 
 
     </Nav>
-    <Form inline>
+    {/* <Form inline>
       <FormControl type="text" placeholder="Search" className="mr-sm-2" />
       
-    </Form>
-    <Nav.Link href="#profile" title="profile"><h1><i class="fas fa-user-circle"></i></h1></Nav.Link>
+    </Form> */}
+    
     <Dropdown  >
-  <Dropdown.Toggle  id="dropdown-basic" toggle-class="rounded-circle px-2">
-    <h1><i class="fas fa-user-circle"></i></h1>
+  <Dropdown.Toggle  id="dropdown-basic" toggle-class="rounded-circle px-2" variant="light">
+    <i class="fas fa-user-circle fa-2x "></i>
   </Dropdown.Toggle>
 
   <Dropdown.Menu>
@@ -33,6 +33,9 @@ const Header = () => {
     <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
   </Dropdown.Menu>
 </Dropdown>
+<Nav.Link href="languages" title="Languages" className="Icon_Hover" ><i class="fas fa-globe fa-2x"></i></Nav.Link>
+<Button variant="danger" className="rounded-circle">Answer</Button>
+
 
     </Container>
   </Navbar>
